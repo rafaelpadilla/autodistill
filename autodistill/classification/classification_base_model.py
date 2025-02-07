3f6533faf39aef4b357051c130eff637d02d82b8
rafaelpadilla <eng.rafaelpadilla@gmail.com>
2025-02-07 18:37:20 +0000

    including optionals in the label(): post processing callback and shelf_file to save partial results. Making predict calls async

--- autodistill/classification/classification_base_model.py
+++ autodistill/classification/classification_base_model.py
@@ -1,7 +1,17 @@
+
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from multiprocessing import Lock, Manager
+
 import glob
 import os
 from abc import abstractmethod
 from dataclasses import dataclass
+import pickle
+import random
+import shelve
+from typing import Callable, List, Optional
+from concurrent.futures import ThreadPoolExecutor
 
 import supervision as sv
 from tqdm import tqdm
@@ -25,32 +35,68 @@ class ClassificationBaseModel(BaseModel):
         """
         pass
 
+    def predicting(self, img_path: str, fn_post_process: Callable = None, shelve_file: str = None):
+        try:
+            detections = self.predict(img_path, fn_post_process=fn_post_process)
+            if isinstance(shelve_file, str):
+                with self.shelf_lock:
+                    with shelve.open(shelve_file) as shelf:
+                        shelf[img_path] = detections
+        except Exception as e:
+            detections = None
+            print(f"Error labeling {img_path}: {e}")
+        return detections
+
     def label(
         self,
         input_folder: str,
         extension: str = ".jpg",
         output_folder: str | None = None,
+        fn_post_process: Callable = None,
+        shelve_file: Optional[str] = None,
+
     ) -> sv.ClassificationDataset:
         """
         Label a dataset and save it in a classification folder structure.
         """
+        input_folder = str(input_folder)
         if output_folder is None:
             output_folder = input_folder + "_labeled"
+        else:
+            output_folder = str(output_folder)
 
         os.makedirs(output_folder, exist_ok=True)
 
-        image_paths = glob.glob(input_folder + "/*" + extension)
-        detections_map = {}
+        image_paths = sorted(glob.glob(input_folder + "/*" + extension))
 
-        progress_bar = tqdm(image_paths, desc="Labeling images")
-        for f_path in progress_bar:
-            progress_bar.set_description(desc=f"Labeling {f_path}", refresh=True)
+        img2results = {}
+        if shelve_file is not None:
+            with shelve.open(shelve_file) as shelf:
+                img2results = dict(shelf)
+        else:
+            shelf = {}
 
-            detections = self.predict(f_path)
-            detections_map[f_path] = detections
+        image_paths = [p for p in image_paths if p not in img2results]
+        # Shuffle the image paths
+        random.shuffle(image_paths)
+        print(f"Started labeling {len(image_paths)} images")
+
+        desc = "Labeling images"
+        max_workers = os.cpu_count()
+        self.shelf_lock = Lock()
+        with ThreadPoolExecutor(max_workers=max_workers) as pool:
+            p = partial(self.predicting, fn_post_process=fn_post_process, shelve_file=shelve_file)
+            list(tqdm(
+                pool.map(p, image_paths),
+                total=len(image_paths),
+                desc=desc,
+            ))
+
+        with shelve.open(shelve_file) as shelf:
+            detections_map = dict(shelf)
 
         dataset = sv.ClassificationDataset(
-            self.ontology.classes(), image_paths, detections_map
+            self.ontology.classes(), list(detections_map.keys()), detections_map
         )
 
         train_cs, test_cs = dataset.split(
